a3ae1add2635d17149201ccca7d38308de333fd4
AlexTheVeil <98246348+AlexTheVeil@users.noreply.github.com>
2025-10-29 13:14:27 -0500

    Edited shopping page

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 def product_list(request):
-    return render(request, "shop/product_list.html")
+    products = Product.objects.all()
+    return render(request, "shop/product_list.html", {"products": products})  
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, "shop/product_detail.html", {"product": product})
+    
